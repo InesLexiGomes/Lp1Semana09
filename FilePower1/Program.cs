a97ad80fa0186a2134162bf084d2332a20ab86c2
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-10 15:18:40 +0100

    Created FilePower that reads the lines and adds them to a Queue

---
+++ FilePower1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();            
+            string input = "Start!"
+            while (input != "")
+            {
+                input = Console.ReadLine();
+                queue.Enqueue(input);
+            }
+        }
+    }
+}
