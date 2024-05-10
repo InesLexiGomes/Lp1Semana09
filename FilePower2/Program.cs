f4ce68c037cf5b868935e6b68c5ccdf72568f12e
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-10 15:39:16 +0100

    Created FilePower2 which writes directly onto the file instead of saving into a queue

---
+++ FilePower2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FilePower2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = $"FilePower2/{args[0]}";
+            StreamWriter sw = new StreamWriter(path);
+
+            string input = "Start!";
+            while (input != "")
+            {
+                input = Console.ReadLine();
+                sw.WriteLine(input);
+            }
+            sw.Close();
+        }
+    }
+}
